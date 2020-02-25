bfdf91d353f3a3a4e244bd79961e595094335a38
Hans Goor <rooteddev@outlook.com>
2020-02-25 11:44:23 +0100

    Made the base class with the plugin.yml.

---
+++ src/main/java/org/gamecie/spleefcie/SpleefCie.java
@@ -0,0 +1,12 @@
+package org.gamecie.spleefcie;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SpleefCie extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("HELLO WORLD I AM ALIVE!");
+    }
+
+}
